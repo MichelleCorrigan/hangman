5f37141762dbd2605183f62bdf3cdc287733aa5c
lalCorr <lal.corrigan@gmail.com>
2022-08-22 18:26:21 +0000

    Import random, and add get word function

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+animal_words = ["Alligator", "Bear", "Cheetah", "Deer"]
+
+def get_word():
+    word = random.choice(animal_words)
+    print(word)
+
+get_word()
