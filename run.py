5746c17a75c8715f9fbddc661f14af34839cb43e
lalCorr <lal.corrigan@gmail.com>
2022-08-23 14:49:40 +0000

    Add play_game function and main function

--- run.py
+++ run.py
@@ -4,14 +4,35 @@ animal_words = ["Alligator", "Bear", "Cheetah", "Deer"]
 
 def get_word():
     word = random.choice(animal_words)
-    print(word)
-
-get_word()
+    return word
 
 
-def letters_guessed():
+def play_game(word):
+    player = input("Enter your name: ")
+    print(f"Hello {player}! Let's play Hangman!\n ")
+    word_to_guess = "_" * len(word)
+    print(word_to_guess)
+    guess = input("Please guess a letter: ")
+    guessed = False
+    guessed_letters = []
     num_lives = 7
-    if wrong_guesses < 7:
-        print("Try another guess")
-    else wrong_guesses >= 7:
-        print("No lives left. Game over!")
+
+
+# def letters_guessed():
+#     num_lives = 7
+#     if wrong_guesses < 7:
+#         print("Try another guess")
+#     else wrong_guesses >= 7:
+#         print("No lives left. Game over!")
+
+
+def main():
+    """
+    Run all program functions
+    """
+    get_word()
+    play_game(word)
+
+
+
+print("Welcome to Hangman!")
