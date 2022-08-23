35aa07f8ca3d6dd09ec7a7a81d3a057991658560
lalCorr <lal.corrigan@gmail.com>
2022-08-23 15:46:31 +0000

    Add to play_game function

--- run.py
+++ run.py
@@ -7,15 +7,28 @@ def get_word():
     return word
 
 
-def play_game(word):
-    player = input("Enter your name: ")
+def play_game(get_word):
+    player = input("Enter your name: ").upper()
     print(f"Hello {player}! Let's play Hangman!\n ")
-    word_to_guess = "_" * len(word)
-    print(word_to_guess)
-    guess = input("Please guess a letter: ")
     guessed = False
     guessed_letters = []
     num_lives = 7
+    word_to_guess = "_" * len(get_word())
+    print(word_to_guess)
+    guess = input("Please guess a letter: ").upper()
+        if guess not in word_to_guess:
+        print(f"Sorry! There is no {guess}.")
+        num_lives -= 1
+        print(f"Lives left: {num_lives}")
+        print(input("Please guess again: ").upper())
+        
+    else:
+        print(f"Correct! {guess} is in {word_to_guess}")
+        
+
+
+play_game(get_word)
+    
 
 
 # def letters_guessed():
@@ -26,13 +39,13 @@ def play_game(word):
 #         print("No lives left. Game over!")
 
 
-def main():
-    """
-    Run all program functions
-    """
-    get_word()
-    play_game(word)
+# def main():
+#     """
+#     Run all program functions
+#     """
+#     get_word()
+#     play_game(word)
 
 
 
-print("Welcome to Hangman!")
+# print("Welcome to Hangman!")
