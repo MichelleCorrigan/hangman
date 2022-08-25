0445316de2d30fa9ce7951393ef28052b67c8eea
lalCorr <lal.corrigan@gmail.com>
2022-08-25 14:14:58 +0000

    Add if/else code to deal with incorrect input

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ def get_word():
 
 
 def play_game(word):
+    print("Try to guess the word:")
     word_to_guess = "_" * len(word)
     guessed = False
     guessed_letters = []
@@ -25,29 +26,43 @@ def play_game(word):
         print("")
     
         guess = input("Please guess a letter: ").upper()
-        if guess in guessed_letters:
-            print(f"You already guessed {guess}")
-        elif guess not in word:
-            print(f"Sorry! There is no {guess}.")
-            guessed_letters.append(guess)
-            print(f"Letters already guessed: {guessed_letters}")
-            num_lives -= 1
-            print(f"Lives left: {num_lives}")
-            if num_lives == 0:
-                print("Game over!")
-                break
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print(f"You already guessed {guess}")
+            elif guess not in word:
+                print(f"Sorry! There is no {guess}.")
+                guessed_letters.append(guess)
+                print(f"Letters already guessed: {guessed_letters}")
+                num_lives -= 1
+                print(f"Lives left: {num_lives}")
+                if num_lives == 0:
+                    print("Game over!")
+                    break
+            else:
+                print("Correct!")
+                guessed_letters.append(guess)
+                letters_in_word = list(word_to_guess)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    letters_in_word[index] = guess
+                word_to_guess = "".join(letters_in_word)
+                if "_" not in word_to_guess:
+                    print(f"You win! The word was {word}!")
+                    guessed = True
         else:
-            print("Correct!")
-            guessed_letters.append(guess)
-            letters_in_word = list(word_to_guess)
-            indices = [i for i, letter in enumerate(word) if letter == guess]
-            for index in indices:
-                letters_in_word[index] = guess
-            word_to_guess = "".join(letters_in_word)
-            if "_" not in word_to_guess:
-                print(f"You win! The word was {word}!")
-                guessed = True
-            
+            print("Incorrect input. Please enter a single letter.")
+
+
+def play_loop():
+    play_again = input("Play again: Y/N? ").upper
+    if play_again == ("Y"):
+        print("Great!")
+        main()
+        # else:if play_again == "n":
+    else:
+        print("Thanks for playing!")
+        exit()
+
 
 def main():
     """
@@ -55,9 +70,9 @@ def main():
     """
     word = get_word()
     play_game(word)
-
+    play_loop()
 
 print("Welcome to Hangman!")
 player = input("Enter your name: ").upper()
-print(f"Hello {player}! Let's play Hangman!\n ")
+print(f"Hello {player}! Let's play!\n ")
 main()
